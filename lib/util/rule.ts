62fad7df0eaf5cae94fe7d8aa4aedb171761e391
Toru Nagashima <public@mysticatea.dev>
2020-05-25 21:32:17 +0900

    🐛 fix docs url

--- lib/util/rule.ts
+++ lib/util/rule.ts
@@ -1,7 +1,6 @@
 import { ESLintUtils } from "@typescript-eslint/experimental-utils"
-import { version } from "../../package.json"
 
 // eslint-disable-next-line new-cap
 export const rule = ESLintUtils.RuleCreator(ruleName =>
-    `https://github.com/mysticatea/eslint-plugin-dprint/blob/v${version}/docs/rules/${ruleName}.md`
+    `https://github.com/mysticatea/eslint-plugin-dprint/blob/master/docs/rules/${ruleName}.md`
 )
